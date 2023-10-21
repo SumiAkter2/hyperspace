b8483c4dc30efefc2c84444e9e3337741197f8ba
SumiAkter2 <akteralisha36@gmail.com>
2023-10-21 10:45:32 +0600

    added sidebar for phone

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  console.log("dd");
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
